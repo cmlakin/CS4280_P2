c7648679ad43c7a4b5df33bf0334ebaa041733d4
Corrina Lakin <corrina.lakin@gmail.com>
2022-03-20 17:30:58 -0500

    added parser.cpp starting code

--- parser.cpp
+++ parser.cpp
@@ -1,89 +1,51 @@
-// call scanner and  print  token until scaner returns EOF
-//#include "token.h"
-#include "scanner.h"
-#include "testScanner.h"
+node_t * parser() {
 
-using namespace std;
+  node_t* root;
+  tk = scanner();
+  root = S();
 
-void testScanner(istream& in) {
-
-  Token token;
-  string fileLine;
-  int lineNum = 0;
-
-
-  if (in.eof()) {
-    cout << "Error: Empty file, no content.\n";
-    cout << "Exiting program.\n";
+  if (tk.ID == EOFtk){
+    continue;
   }
-  else if(!in.eof()) {
-
-    while(getline(in, fileLine)) {
-      //cout << "fileLine= " << fileLine << endl;
-      lineNum++;
-
-      while (fileLine.length() > 0) {
-
-        // pass line and line number to scanner
-        token = scanner(fileLine, lineNum);
-        //scanner returns token info - print info
-        if (token.tokenID == -1) {
-          cout << "ERROR: Invalid character: " << token.chars << endl;
-          cout << "Exiting program.\n\n";
-          return;
-        }
-        else if (token.tokenID == -2) {
-          cout << "ERROR: Invalid character on line #: "<< token.line << endl;
-          cout << "String not keyword: " << token.chars << endl;
-          cout << "Exiting program.\n\n";
-          return;
-        }
-        else if (token.tokenID == 500) {
-          // do not print for whitespace
-          continue;
-        }
-        else if (token.tokenID == 1006){
-          // do not print for comment
-          continue;
-        }
-        else if (token.tokenID == 1007){
-          cout << "End of input.\nExiting Program.\n";
-          return;
-        }
-        else {
-          // print token information
-          printTokenType(token.tokenID, token.line, token.chars);
-        }
-      }
-    }
-
-    // // set when the end of the file has been reached.
-    token.line = lineNum;
-    token.tokenID = 1003;
-    token.chars = "EOF";
-    printTokenType(token.tokenID, token.line, token.chars);
+  else {
+    cout << "Error. Exiting program.";
   }
 
-  return;
+  return root;
 }
 
-void printTokenType(int tkId, int tkLn, string tkStr) {
-  int index = tkId - 1001;
-  cout << "Line number: " << tkLn << endl;
-  cout << tokenNames[index] << ": " << tkStr <<endl;
+node_t* S(){
 
+  node_t* p = getNode(S);
+  if (tk.ID == b) { // predict S->bA since bc First(bAa)
+    p->token1 = tk; // b needed to be stored
+    tk = scanner(); // processing b consume matching tokens
+    p->child = A(); // processing A
 
-  switch(index) {
-    case 0:  cout << "KW_tk\n\n";
-    break;
-    case 1:  cout << "ID_tk\n\n";
-    break;
-    case 2:  cout << "EOF_tk\n\n";
-    break;
-    case 3:  cout << "NUM_tk\n\n";
-    break;
-    case 4:  cout << "OP_tk\n\n";
-    break;
+    return p;  // explicit return
+  }
+  else if (tk.ID == c) { //predict S->c
+    tk = scanner(); // consume c no need to store
 
+    return p;  // explicit return
+}
+
+node_t* A() {
+
+  if (tk.ID == d) {  // predicts A->dSa
+    node_t* p = getNode(A); // now we need node
+    p->token1 = tk; // d needed to be stored
+    tk = scanner(); // processing d
+    p->child = S(); // processing S
+
+    if (tk.ID == a) {  // processing a
+      tk = scanner();
+    }
+    else {
+      cout << "Error. Exiting program.";
+    }
+  }
+  else {  // predicts A->(epslon)
+    return NULL;
   }
 }
